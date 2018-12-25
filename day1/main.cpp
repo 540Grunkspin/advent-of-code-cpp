34f65107853cce2c997370b78f3f82a01a49cc90
Anton Holmberg <anton.holmberg@greenely.se>
2018-12-25 18:15:15 +0100

    Reorder functions

--- day1/main.cpp
+++ day1/main.cpp
@@ -8,39 +8,40 @@
 #include <vector>
 
 std::vector<std::string> get_input(char const *const file) {
-    std::ifstream ifs{file};
+    int main(int argc, char *argv[]) {
+        auto const input = get_input(argv[1]);
+        std::vector<int> numbers;
+        numbers.resize(input.size());
+        std::transform(input.begin(), input.end(), numbers.begin(), [](auto s) { return atoi(s.c_str()); });
 
-    std::string tmp_in;
-    std::vector<std::string> lines;
-    while (std::getline(ifs, tmp_in, '\n')) {
-        lines.push_back(tmp_in);
+        std::unordered_set<int> frequencies;
+        int current_freq = 0;
+
+        bool did_insert;
+        do {
+            for (int &number : numbers) {
+                current_freq += number;
+                did_insert = frequencies.insert(current_freq).second;
+                if (!did_insert) {
+                    break;
+                }
+            }
+        } while (did_insert);
+
+        std::cout << "Exercise 1: " << std::accumulate(numbers.begin(), numbers.end(), 0) << '\n';
+        std::cout << "Exercise 2: " << current_freq;
+
+        return 0;
     }
 
-    return lines;
-}
+    std::vector<std::string> get_input(char const *const file) {
+        std::ifstream ifs{file};
 
-int main(int argc, char *argv[]) {
-    auto const input = get_input(argv[1]);
-    std::vector<int> numbers;
-    numbers.resize(input.size());
-    std::transform(input.begin(), input.end(), numbers.begin(), [](auto s) { return atoi(s.c_str()); });
-
-    std::unordered_set<int> frequencies;
-    int current_freq = 0;
-
-    bool did_insert;
-    do {
-        for (int &number : numbers) {
-            current_freq += number;
-            did_insert = frequencies.insert(current_freq).second;
-            if (!did_insert) {
-                break;
-            }
+        std::string tmp_in;
+        std::vector<std::string> lines;
+        while (std::getline(ifs, tmp_in, '\n')) {
+            lines.push_back(tmp_in);
         }
-    } while (did_insert);
 
-    std::cout << "Exercise 1: " << std::accumulate(numbers.begin(), numbers.end(), 0) << '\n';
-    std::cout << "Exercise 2: " << current_freq;
-
-    return 0;
-}
+        return lines;
+    }
